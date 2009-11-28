0eb5c8654ad33cf2cfd35d1ab6409f73e0134111
Ralph Eastwood <tcmreastwood@ntlworld.com>
2009-11-28 06:52:29 +0000

    Removed commented constants they can conflict

--- include/gmath/constants.h
+++ include/gmath/constants.h
@@ -12,19 +12,4 @@
 
 #define PI 3.141592653589793238462643383276
 
-/*
-#ifndef X
-#define X 0
-#endif
-#ifndef Y
-#define Y 1
-#endif
-#ifndef Z
-#define Z 2
-#endif
-#ifndef W
-#define W 3
-#endif
-*/
-
 #endif /* _GMATH_CONSTANTS_H_ */
